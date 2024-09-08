8e6f80fd655b565aec59daeace40b5e857ecd0e1
mark johnson <mjohnson24@comcast.net>
2024-09-08 18:06:56 -0500

    added first slider input to DOM to make sure it works

--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@
 
 window.loadSlider = function (json) {
 
+	const slider = document.createElement("input");
+	slider.type = "range";
+	slider.className = "form-range pb-5 pt-5";
+	slider.min = 1;
+	slider.max = 20;
+	slider.value = 17;
+	slider.id = 1234;
+	
 
-
+	const div = document.getElementById("slider");
+	div.appendChild(slider);
   
 }
