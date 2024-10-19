31213cfb4c116968dfeb1c0c2ca4259e1835879c
mark johnson <mjohnson24@comcast.net>
2024-10-19 18:03:42 -0500

    added displaying the value next to question

--- src/index.js
+++ src/index.js
@@ -2,17 +2,29 @@
 
 
 window.loadSlider = function (json) {
+	
+	const div = document.getElementById("slider");
 
+	// create a slider
 	const slider = document.createElement("input");
 	slider.type = "range";
-	slider.className = "form-range pb-5 pt-5";
+	slider.className = "form-range pb-5";
 	slider.min = 1;
 	slider.max = 20;
 	slider.value = 17;
 	slider.id = 1234;
-	
+	slider.oninput = function() {
+		console.log(this.value);
+		const divUpdate = document.getElementById("sliderValueDiv");
+		divUpdate.innerHTML = this.value;
+	}
 
-	const div = document.getElementById("slider");
+	// create a question
+	const questionDiv = document.createElement("p");
+	questionDiv.innerHTML = `How old are you? <span id="sliderValueDiv" class="bg-primary text-white rounded p-1">5</span>`;
+
+	// appending to div
+	div.appendChild(questionDiv);
 	div.appendChild(slider);
   
 }
