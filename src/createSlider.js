bfd77a9355543d457357dafb41fb8935800abcec
mark johnson <mjohnson24@comcast.net>
2024-10-20 15:37:17 -0500

    Refactor Code to put in its own createSlider.js file

---
+++ src/createSlider.js
@@ -0,0 +1,43 @@
+const createSlider = function(question) {
+	// console.log(question);
+	const fieldData = question?.fieldData || {};
+	const text = fieldData?.Question;
+	const minValue = fieldData?.MinValue;
+	const maxValue = fieldData?.MaxValue;
+	const Value = fieldData?.Value;
+	const id = fieldData?.PrimaryKey;
+	
+
+
+	const slider = document.createElement("input");
+	slider.type = "range";
+	slider.className = "form-range pb-5";
+	slider.min = minValue;
+	slider.max = maxValue;
+	slider.value = Value;
+	slider.id = id;
+	slider.oninput = function() {
+		console.log(this.value);
+		const divUpdate = document.getElementById(`sliderValueDiv_${id}`);
+		divUpdate.innerHTML = this.value;
+	}
+	slider.onchange = function() {
+		console.log(this.value);
+		const value = this.value;
+		const id = this.id;
+		const obj = { value, id };
+		console.log(obj);
+		FileMaker.PerformScript("UpdateField", JSON.stringify(obj));
+	}
+
+	// create a question
+	const questionDiv = document.createElement("p");
+	questionDiv.innerHTML = `${text} <span id="sliderValueDiv_${id}" class="bg-primary text-white rounded p-1">5</span>`;
+
+	const sliderDiv = document.createElement("div");
+	sliderDiv.appendChild(questionDiv);
+	sliderDiv.appendChild(slider);
+	return sliderDiv;
+}
+
+export default createSlider;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-
+import createSlider from "./createSlider";
 
 
 window.loadSlider = function (json) {
@@ -10,49 +10,12 @@ window.loadSlider = function (json) {
 
 	const div = document.getElementById("slider");
 	// create a slider
-	const createSlider = function(question) {
-		// console.log(question);
-		const fieldData = question?.fieldData || {};
-		const text = fieldData?.Question;
-		const minValue = fieldData?.MinValue;
-		const maxValue = fieldData?.MaxValue;
-		const Value = fieldData?.Value;
-		const id = fieldData?.PrimaryKey;
-		
-
-
-		const slider = document.createElement("input");
-		slider.type = "range";
-		slider.className = "form-range pb-5";
-		slider.min = minValue;
-		slider.max = maxValue;
-		slider.value = Value;
-		slider.id = id;
-		slider.oninput = function() {
-			console.log(this.value);
-			const divUpdate = document.getElementById(`sliderValueDiv_${id}`);
-			divUpdate.innerHTML = this.value;
-		}
-		slider.onchange = function() {
-			console.log(this.value);
-			const value = this.value;
-			const id = this.id;
-			const obj = { value, id };
-			console.log(obj);
-			FileMaker.PerformScript("UpdateField", JSON.stringify(obj));
-		}
-
-		// create a question
-		const questionDiv = document.createElement("p");
-		questionDiv.innerHTML = `${text} <span id="sliderValueDiv_${id}" class="bg-primary text-white rounded p-1">5</span>`;
-
-		// appending to div
-		div.appendChild(questionDiv);
-		div.appendChild(slider);
-	}
+	
 
 	data.forEach(function(question) {
-		createSlider(question);
+		const slider = createSlider(question);
+		// appending to div
+		div.appendChild(slider);
 	});
 
 	//createSlider();
