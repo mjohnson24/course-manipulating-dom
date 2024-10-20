abbaf23275642b0e39a30d514d3aefb20e88dccd
mark johnson <mjohnson24@comcast.net>
2024-10-20 15:19:42 -0500

    Update each question sliders value with unique id

--- src/index.js
+++ src/index.js
@@ -10,17 +10,27 @@ window.loadSlider = function (json) {
 
 	const div = document.getElementById("slider");
 	// create a slider
-	const createSlider = function() {
+	const createSlider = function(question) {
+		// console.log(question);
+		const fieldData = question?.fieldData || {};
+		const text = fieldData?.Question;
+		const minValue = fieldData?.MinValue;
+		const maxValue = fieldData?.MaxValue;
+		const Value = fieldData?.Value;
+		const id = fieldData?.PrimaryKey;
+		
+
+
 		const slider = document.createElement("input");
 		slider.type = "range";
 		slider.className = "form-range pb-5";
-		slider.min = 1;
-		slider.max = 20;
-		slider.value = 17;
-		slider.id = 1234;
+		slider.min = minValue;
+		slider.max = maxValue;
+		slider.value = Value;
+		slider.id = id;
 		slider.oninput = function() {
 			console.log(this.value);
-			const divUpdate = document.getElementById("sliderValueDiv");
+			const divUpdate = document.getElementById(`sliderValueDiv_${id}`);
 			divUpdate.innerHTML = this.value;
 		}
 		slider.onchange = function() {
@@ -34,15 +44,15 @@ window.loadSlider = function (json) {
 
 		// create a question
 		const questionDiv = document.createElement("p");
-		questionDiv.innerHTML = `How old are you? <span id="sliderValueDiv" class="bg-primary text-white rounded p-1">5</span>`;
+		questionDiv.innerHTML = `${text} <span id="sliderValueDiv_${id}" class="bg-primary text-white rounded p-1">5</span>`;
 
 		// appending to div
 		div.appendChild(questionDiv);
 		div.appendChild(slider);
 	}
 
-	data.forEach(function() {
-		createSlider();
+	data.forEach(function(question) {
+		createSlider(question);
 	});
 
 	//createSlider();
