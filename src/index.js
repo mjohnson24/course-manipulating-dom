ab92650e37fa419ffb05d153a8995494430023ff
mark johnson <mjohnson24@comcast.net>
2024-10-20 14:43:20 -0500

    Moved creating slider into its own function

--- src/index.js
+++ src/index.js
@@ -6,30 +6,36 @@ window.loadSlider = function (json) {
 	const div = document.getElementById("slider");
 
 	// create a slider
-	const slider = document.createElement("input");
-	slider.type = "range";
-	slider.className = "form-range pb-5";
-	slider.min = 1;
-	slider.max = 20;
-	slider.value = 17;
-	slider.id = 1234;
-	slider.oninput = function() {
-		console.log(this.value);
-		const divUpdate = document.getElementById("sliderValueDiv");
-		divUpdate.innerHTML = this.value;
-	}
-	slider.onchange = function() {
-		console.log(this.value);
-		const divUpdate = document.getElementById("sliderValueDiv");
-		divUpdate.innerHTML = this.value;
-	}
+	const createSlider = function() {
+		const slider = document.createElement("input");
+		slider.type = "range";
+		slider.className = "form-range pb-5";
+		slider.min = 1;
+		slider.max = 20;
+		slider.value = 17;
+		slider.id = 1234;
+		slider.oninput = function() {
+			console.log(this.value);
+			const divUpdate = document.getElementById("sliderValueDiv");
+			divUpdate.innerHTML = this.value;
+		}
+		slider.onchange = function() {
+			console.log(this.value);
+			const value = this.value;
+			const id = this.id;
+			const obj = { value, id };
+			console.log(obj);
+			FileMaker.PerformScript("UpdateField", JSON.stringify(obj));
+		}
 
-	// create a question
-	const questionDiv = document.createElement("p");
-	questionDiv.innerHTML = `How old are you? <span id="sliderValueDiv" class="bg-primary text-white rounded p-1">5</span>`;
+		// create a question
+		const questionDiv = document.createElement("p");
+		questionDiv.innerHTML = `How old are you? <span id="sliderValueDiv" class="bg-primary text-white rounded p-1">5</span>`;
 
-	// appending to div
-	div.appendChild(questionDiv);
-	div.appendChild(slider);
+		// appending to div
+		div.appendChild(questionDiv);
+		div.appendChild(slider);
+	}
+	createSlider();
   
 }
